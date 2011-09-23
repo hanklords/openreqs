49992aec934152bf9e45fe999b056d25cabf3a4e
Mael Clerambault <mael@clerambault.fr>
2011-09-23 18:27:49 +0200

    Simplify creole extension and remove requirement link to docs

--- openreqs.rb
+++ openreqs.rb
@@ -13,24 +13,16 @@ class DocReqParser < Creole::Parser
     super(@doc["_content"], options)
   end
   
-  def make_local_link(link)
-    if DB["requirements"].find_one("_name" => link)
-      super(link)
-    else
-      super("#{@doc["_name"]}/#{link}/add")
-    end
-  end
-  
   def make_explicit_anchor(uri, text)
-    if uri =~ /\.req$/ and req = DB["requirements"].find_one("_name" => uri)
-      ReqParser.new(req).to_html
+    if req = DB["requirements"].find_one("_name" => uri)
+      ReqParser.new(@doc, req).to_html
     else
-      super
+      super(escape_url(@doc["_name"]) + "/" + escape_url(uri) + "/add", text)
     end
   end
 end
 
-class IndexDocReqParser < DocReqParser
+class IndexDocReqParser < Creole::Parser
   def make_local_link(link)
     if DB["docs"].find_one("_name" => link)
       escape_url(link)
@@ -41,15 +33,15 @@ class IndexDocReqParser < DocReqParser
 end
 
 class ReqParser < Creole::Parser
-  def initialize(req, options = {})
-    @req = req
+  def initialize(doc, req, options = {})
+    @doc, @req = doc, req
     super(@req["_content"], options)
   end
 
   def to_html
     attributes = @req.reject {|k,v| k =~ /^_/}
     "<h2>#{@req["_name"]}</h2><ul>" + 
-        "<a href=\"#{@req["_doc"]}/#{@req["_name"]}/edit\">edit</a><br/>" + 
+        "<a href=\"#{@doc["_name"]}/#{@req["_name"]}/edit\">edit</a><br/>" + 
         attributes.map {|k,v| "<li>#{k}: #{v}</li>"}.join + "</ul>" + super
   end
 end
@@ -62,12 +54,11 @@ get '/' do
   doc = DB["docs"].find_one("_name" => 'index')
   return not_found if doc.nil?
   
-  parser = IndexDocReqParser.new(doc)
+  parser = IndexDocReqParser.new(doc["_content"])
   "<a href=\"index/edit\">edit</a><br/>" + parser.to_html
 end
 
 post '/index/edit' do
-  p params[:content]
   DB["docs"].update({"_name" => 'index'}, {"$set" => {"_content" => params[:content]}})
   redirect to('/')
 end
@@ -75,6 +66,7 @@ end
 get '/:doc' do
   doc = DB["docs"].find_one("_name" => params[:doc])
   return not_found if doc.nil?
+
   parser = DocReqParser.new(doc)
   "<a href=\"#{params[:doc]}/edit\">edit</a><br/>" + parser.to_html
 end
@@ -101,7 +93,6 @@ get '/:doc/edit' do
   return not_found if doc.nil?
   
   @_content = doc["_content"]
-  p @_content
   haml %q{
 %form(method="post")
   %textarea(name="content" cols=80 rows=40)= @_content
@@ -125,20 +116,20 @@ get '/:doc/:req/add' do
 end
 
 post '/:doc/:req/add' do
-  req = {"_name" => params[:req], "_doc" => params[:doc], "_content" => params[:content]}
+  req = {"_name" => params[:req], "_content" => params[:content]}
   DB["requirements"].insert req
   
   redirect to('/' + params[:doc])
 end
 
 post '/:doc/:req/delete' do
-  DB["requirements"].remove("_name" => params[:req], "_doc" => params[:doc])
+  DB["requirements"].remove("_name" => params[:req])
   
   redirect to('/' + params[:doc])
 end
 
 get '/:doc/:req/edit' do
-  doc = DB["requirements"].find_one("_doc" => params[:doc], "_name" => params[:req])
+  doc = DB["requirements"].find_one("_name" => params[:req])
   @_content = doc["_content"]
   haml %q{
 %form(method="post")
@@ -152,7 +143,7 @@ get '/:doc/:req/edit' do
 end
 
 post '/:doc/:req/edit' do
-  DB["requirements"].update({"_doc" => params[:doc], "_name" => params[:req]}, {"$set" => {"_content" => params[:content]}})
+  DB["requirements"].update({"_name" => params[:req]}, {"$set" => {"_content" => params[:content]}})
   
   redirect to('/' + params[:doc])
 end
