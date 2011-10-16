5aa0798feaa84262db5f6b8027db838675587045
Mael Clerambault <mael@clerambault.fr>
2011-10-16 18:25:07 +0200

    test index creation on empty db

--- spec/first_spec.rb
+++ spec/first_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
-describe "something" do
-  it "does something" do
+describe "An empty Openreqs application", :type => :request do
+  before(:all) do
+    @db = page.app.mongo
+    @db.connection.drop_database("openreqs")
+    @docs, @requirements = @db["docs"], @db["requirements"]
+  end
+  
+  it "creates an empty index page on first access" do
+    visit '/'
+    @docs.find.to_a.should have(1).items
+    index = @docs.find_one
+    index.should include("date", "_content", "_name")
+    index["_name"].should == "index"
+    index["_content"].should be_empty
+    index["date"].should be_within(60).of(Time.now)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ lib = File.expand_path('../..', __FILE__)
 $:.unshift lib unless $:.include?(lib)
 
 require 'capybara/rspec'
+require 'rspec'
 require 'openreqs'
+
+set :environment, :test
+
+Capybara.app = Sinatra::Application
